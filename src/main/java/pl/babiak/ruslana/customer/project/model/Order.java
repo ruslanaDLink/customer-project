ac9338bfcfe63671d65170172b6849050e7c347c
Ruslana Babiak <ruslana.babiak@gmail.com>
2024-01-26 14:46:28 +0100

    Added list of products to order.

--- src/main/java/pl/babiak/ruslana/customer/project/model/Order.java
+++ src/main/java/pl/babiak/ruslana/customer/project/model/Order.java
@@ -1,13 +1,16 @@
 package pl.babiak.ruslana.customer.project.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Order {
     private long id;
     private LocalDateTime date;
     private double price;
+    private List<Product> products;
 
-    public Order(long id, LocalDateTime date, double price) {
+    public Order(long id, List<Product> products, LocalDateTime date, double price) {
+        this.products = products;
         this.id = id;
         this.date = date;
         this.price = price;
@@ -19,6 +22,14 @@ public class Order {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
     }
 
     public LocalDateTime getDate() {
