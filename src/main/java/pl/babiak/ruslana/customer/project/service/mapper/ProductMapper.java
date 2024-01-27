2e2bc3bb6c3caee5e8e320c82afb59ecda8658b1
Ruslana Babiak <ruslana.babiak@gmail.com>
2024-01-27 11:14:55 +0100

    Added map() method and provided its functionality to controller and service.

--- src/main/java/pl/babiak/ruslana/customer/project/service/mapper/ProductMapper.java
+++ src/main/java/pl/babiak/ruslana/customer/project/service/mapper/ProductMapper.java
@@ -10,4 +10,12 @@ public class ProductMapper {
     public Product map(ProductEntity entity) {
         return new Product(entity.getId(), entity.getProduct(), entity.getCost());
     }
+
+    public ProductEntity map(Product entity) {
+        ProductEntity productEntity = new ProductEntity();
+        productEntity.setId(entity.getId());
+        productEntity.setProduct(entity.getProduct());
+        productEntity.setCost(entity.getCost());
+        return productEntity;
+    }
 }
